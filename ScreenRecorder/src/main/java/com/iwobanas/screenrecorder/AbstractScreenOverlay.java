655b707f70849f65832ccaec4faa871a9c5dc39f
Iwo Banas <banas.iwo@gmail.com>
2013-05-21 12:27:40 +0200

    extract common screen overlays logic to abstract superclass

---
+++ ScreenRecorder/src/main/java/com/iwobanas/screenrecorder/AbstractScreenOverlay.java
@@ -0,0 +1,62 @@
+package com.iwobanas.screenrecorder;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+
+public abstract class AbstractScreenOverlay implements IScreenOverlay {
+
+    private static final String TAG = "AbstractScreenOverlay";
+
+    private Context mContext;
+
+    private View mView;
+
+    private boolean visible = false;
+
+    public AbstractScreenOverlay(Context context) {
+        mContext = context;
+    }
+
+    protected abstract View createView();
+
+    protected abstract WindowManager.LayoutParams getLayoutParams();
+
+    protected LayoutInflater getLayoutInflater() {
+        return (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    protected Context getContext() {
+        return mContext;
+    }
+
+    @Override
+    public void show() {
+        if (visible) {
+            Log.e(TAG, "Trying to show the view which is already displayed");
+            return;
+        }
+        if (mView == null) {
+            mView = createView();
+        }
+
+        getWindowManager().addView(mView, getLayoutParams());
+        visible = true;
+    }
+
+    private WindowManager getWindowManager() {
+        return (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+    }
+
+    @Override
+    public void hide() {
+        if (!visible) {
+            Log.e(TAG, "Trying to hide the view which is not displayed");
+            return;
+        }
+        getWindowManager().removeView(mView);
+        visible = false;
+    }
+}
--- ScreenRecorder/src/main/java/com/iwobanas/screenrecorder/WatermarkOverlay.java
+++ ScreenRecorder/src/main/java/com/iwobanas/screenrecorder/WatermarkOverlay.java
@@ -2,25 +2,22 @@ package com.iwobanas.screenrecorder;
 
 import android.content.Context;
 import android.graphics.PixelFormat;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 
-public class WatermarkOverlay implements IScreenOverlay {
-
-    private Context mContext;
-
-    private View mView;
+public class WatermarkOverlay extends AbstractScreenOverlay {
 
     public WatermarkOverlay(Context context) {
-        mContext = context;
+        super(context);
     }
 
     @Override
-    public void show() {
-        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        mView = inflater.inflate(R.layout.watermark, null);
+    protected View createView() {
+        return getLayoutInflater().inflate(R.layout.watermark, null);
+    }
 
+    @Override
+    protected WindowManager.LayoutParams getLayoutParams() {
         WindowManager.LayoutParams lp = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.MATCH_PARENT,
                 WindowManager.LayoutParams.MATCH_PARENT);
@@ -29,14 +26,7 @@ public class WatermarkOverlay implements IScreenOverlay {
                 | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
                 | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
         lp.format = PixelFormat.TRANSLUCENT;
-        lp.setTitle(mContext.getString(R.string.app_name));
-
-        WindowManager windowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
-        windowManager.addView(mView, lp);
-    }
-
-    @Override
-    public void hide() {
-
+        lp.setTitle(getContext().getString(R.string.app_name));
+        return lp;
     }
 }
