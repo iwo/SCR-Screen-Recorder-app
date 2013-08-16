3a766a982eda2acdb2857c2dfb7546521c04ef60
Iwo Banas <banas.iwo@gmail.com>
2013-08-16 11:34:37 +0200

    add high frame rates for use with OES

--- ScreenRecorder/src/main/java/com/iwobanas/screenrecorder/settings/FrameRateDialogFragment.java
+++ ScreenRecorder/src/main/java/com/iwobanas/screenrecorder/settings/FrameRateDialogFragment.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.ContextThemeWrapper;
 
@@ -21,9 +22,14 @@ public class FrameRateDialogFragment extends DialogFragment {
         builder.setIcon(R.drawable.ic_launcher);
         builder.setTitle(R.string.settings_frame_rate);
         DecimalFormat upTo = new DecimalFormat(getString(R.string.settings_frame_rate_up_to));
-        final int[] frameRates = new int[]{-1, 30, 20, 15, 10, 5};
-        String[] items = new String[]{getString(R.string.settings_frame_rate_max), upTo.format(30),
-                upTo.format(20), upTo.format(15), upTo.format(10), upTo.format(5)};
+        final int[] frameRates = (Build.VERSION.SDK_INT < 18) ? new int[]{-1, 30, 20, 15, 10, 5} : new int[]{-1, 50, 40, 30, 20, 15, 10, 5};
+        String[] items = new String[frameRates.length];
+
+        items[0] = getString(R.string.settings_frame_rate_max);
+        for (int i = 1; i < frameRates.length; i++) {
+            items[i] = upTo.format(frameRates[i]);
+        }
+
         builder.setItems(items, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
