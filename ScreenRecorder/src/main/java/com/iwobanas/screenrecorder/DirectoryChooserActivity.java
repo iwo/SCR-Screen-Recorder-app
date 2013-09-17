1a4ec826799de9d0cd1e0f64e293715562097921
Iwo Banas <banas.iwo@gmail.com>
2013-09-17 14:02:30 +0200

    new directory dialog

--- ScreenRecorder/src/main/java/com/iwobanas/screenrecorder/DirectoryChooserActivity.java
+++ ScreenRecorder/src/main/java/com/iwobanas/screenrecorder/DirectoryChooserActivity.java
@@ -1,17 +1,23 @@
 package com.iwobanas.screenrecorder;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -21,7 +27,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class DirectoryChooserActivity extends ListActivity {
-
+    private static final String TAG = "scr_DirectoryChooserActivity";
     private File dir;
     private ArrayList<FileWrapper> items = new ArrayList<FileWrapper>();
     private ArrayAdapter<FileWrapper> adapter;
@@ -118,9 +124,59 @@ public class DirectoryChooserActivity extends ListActivity {
             case R.id.directory_chooser_refresh:
                 setDir(dir);
                 return true;
+            case R.id.directory_chooser_new:
+                showNewDirDialog();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
+    private void showNewDirDialog() {
+        new NewDirDialogFragment().show(getFragmentManager(), "NewDirDialog");
+    }
+
+    public void createDir(String name) {
+        File newDir = new File(dir, name);
+        if (!newDir.exists()) {
+            if (!newDir.mkdirs()) {
+                Log.w(TAG, "Can't create directory: " + newDir.getAbsolutePath());
+                new NewDirErrorDialogFragment().show(getFragmentManager(), "NewDirErrorDialog");
+                return;
+            }
+        }
+        setDir(newDir);
+    }
+
+    public static class NewDirDialogFragment extends DialogFragment {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+            final EditText input = new EditText(getActivity());
+            builder.setView(input);
+
+            builder.setTitle(getString(R.string.directory_chooser_new_title));
+            builder.setPositiveButton(R.string.directory_chooser_new_ok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    String name = input.getText().toString();
+                    ((DirectoryChooserActivity) getActivity()).createDir(name);
+                }
+            });
+            builder.setNegativeButton(R.string.directory_chooser_new_cancel, null);
+
+            return builder.create();
+        }
+    }
+
+    public static class NewDirErrorDialogFragment extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(getString(R.string.directory_chooser_error_message));
+            return builder.create();
+        }
+    }
 }
