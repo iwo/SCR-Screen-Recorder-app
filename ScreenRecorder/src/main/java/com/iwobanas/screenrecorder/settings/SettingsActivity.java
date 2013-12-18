caa9731bb68f3a4f4e7f60c1a3fc92340246a995
Iwo Banas <banas.iwo@gmail.com>
2013-12-18 14:40:10 +0100

    up navigation from settings

--- ScreenRecorder/src/main/java/com/iwobanas/screenrecorder/settings/SettingsActivity.java
+++ ScreenRecorder/src/main/java/com/iwobanas/screenrecorder/settings/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.iwobanas.screenrecorder.settings;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -16,6 +19,7 @@ public class SettingsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         Settings.initialize(this);
         super.onCreate(savedInstanceState);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
 
         if (savedInstanceState == null) {
             fragment = new SettingsFragment();
@@ -49,6 +53,12 @@ public class SettingsActivity extends Activity {
             case R.id.settings_send_bug_report:
                 sendBugReport();
                 return true;
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                Intent upIntent = NavUtils.getParentActivityIntent(this);
+                TaskStackBuilder.create(this).addNextIntentWithParentStack(upIntent).startActivities();
+                return true;
+
             default:
                 return super.onOptionsItemSelected(item);
         }
