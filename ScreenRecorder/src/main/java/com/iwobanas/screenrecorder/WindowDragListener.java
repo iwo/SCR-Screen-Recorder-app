4a09d0a48d96aae70dbd1c2a79bb4f2de72b2b46
Iwo Banas <banas.iwo@gmail.com>
2014-01-30 21:35:59 +0100

    activate recorder overlay dragging on long press

--- ScreenRecorder/src/main/java/com/iwobanas/screenrecorder/WindowDragListener.java
+++ ScreenRecorder/src/main/java/com/iwobanas/screenrecorder/WindowDragListener.java
@@ -10,6 +10,7 @@ import android.view.WindowManager;
 class WindowDragListener implements View.OnTouchListener {
     private int dragStartX;
     private int dragStartY;
+    private boolean dragging;
     private WindowManager.LayoutParams params;
 
     private OnWindowDragStartListener startListener;
@@ -27,57 +28,69 @@ class WindowDragListener implements View.OnTouchListener {
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
-        if (motionEvent.getActionMasked() == MotionEvent.ACTION_DOWN) {
-            dragStartX = (int) motionEvent.getX();
-            dragStartY = (int) motionEvent.getY();
-            if (startListener != null) {
-                startListener.onDragStart();
-            }
-            return true;
-        }
-        if (motionEvent.getActionMasked() == MotionEvent.ACTION_MOVE) {
-            Rect frame = new Rect();
-            view.getWindowVisibleDisplayFrame(frame);
+        switch (motionEvent.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+                if (!dragging) {
+                    dragStartX = (int) motionEvent.getX();
+                    dragStartY = (int) motionEvent.getY();
+                    if (startListener != null) {
+                        startListener.onDragStart();
+                    }
+                    dragging = true;
+                    return true;
+                }
 
-            int leftX, topY, centerX, centerY, rightX, bottomY, gravity = 0;
+                Rect frame = new Rect();
+                view.getWindowVisibleDisplayFrame(frame);
 
-            leftX = (int) motionEvent.getRawX() - dragStartX - frame.left;
-            centerX = (int) motionEvent.getRawX() - dragStartX - (frame.centerX() - view.getWidth() / 2);
-            rightX = frame.right - (int) motionEvent.getRawX() - view.getWidth() + dragStartX;
+                int leftX, topY, centerX, centerY, rightX, bottomY, gravity = 0;
 
-            topY = (int) motionEvent.getRawY() - dragStartY - frame.top;
-            centerY = (int) motionEvent.getRawY() - dragStartY - (frame.centerY() - view.getHeight() / 2);
-            bottomY = frame.bottom - (int) motionEvent.getRawY() - view.getHeight() + dragStartY;
+                leftX = (int) motionEvent.getRawX() - dragStartX - frame.left;
+                centerX = (int) motionEvent.getRawX() - dragStartX - (frame.centerX() - view.getWidth() / 2);
+                rightX = frame.right - (int) motionEvent.getRawX() - view.getWidth() + dragStartX;
 
-            if (leftX <= Math.abs(centerX) && leftX <= rightX) {
-                params.x = Math.max(leftX, 0);
-                gravity |= Gravity.LEFT;
-            } else if (rightX <= Math.abs(centerX) && rightX <= leftX) {
-                params.x = Math.max(rightX, 0);
-                gravity |= Gravity.RIGHT;
-            } else {
-                params.x = centerX;
-                gravity |= Gravity.CENTER_HORIZONTAL;
-            }
+                topY = (int) motionEvent.getRawY() - dragStartY - frame.top;
+                centerY = (int) motionEvent.getRawY() - dragStartY - (frame.centerY() - view.getHeight() / 2);
+                bottomY = frame.bottom - (int) motionEvent.getRawY() - view.getHeight() + dragStartY;
 
-            if (topY <= Math.abs(centerY) && topY <= bottomY) {
-                params.y = Math.max(topY, 0);
-                gravity |= Gravity.TOP;
-            } else if (bottomY <= Math.abs(centerY) && bottomY <= topY) {
-                params.y = Math.max(bottomY, 0);
-                gravity |= Gravity.BOTTOM;
-            } else {
-                params.y = centerY;
-                gravity |= Gravity.CENTER_VERTICAL;
-            }
+                if (leftX <= Math.abs(centerX) && leftX <= rightX) {
+                    params.x = Math.max(leftX, 0);
+                    gravity |= Gravity.LEFT;
+                } else if (rightX <= Math.abs(centerX) && rightX <= leftX) {
+                    params.x = Math.max(rightX, 0);
+                    gravity |= Gravity.RIGHT;
+                } else {
+                    params.x = centerX;
+                    gravity |= Gravity.CENTER_HORIZONTAL;
+                }
 
-            params.gravity = gravity;
+                if (topY <= Math.abs(centerY) && topY <= bottomY) {
+                    params.y = Math.max(topY, 0);
+                    gravity |= Gravity.TOP;
+                } else if (bottomY <= Math.abs(centerY) && bottomY <= topY) {
+                    params.y = Math.max(bottomY, 0);
+                    gravity |= Gravity.BOTTOM;
+                } else {
+                    params.y = centerY;
+                    gravity |= Gravity.CENTER_VERTICAL;
+                }
 
-            getWindowManager(view.getContext()).updateViewLayout(view, params);
-            return true;
-        }
-        if (motionEvent.getActionMasked() == MotionEvent.ACTION_UP && endListener != null) {
-            endListener.onDragEnd();
+                params.gravity = gravity;
+
+                getWindowManager(view.getContext()).updateViewLayout(view, params);
+                return true;
+
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                if (dragging) {
+                    dragging = false;
+                    if (endListener != null) {
+                        endListener.onDragEnd();
+                    }
+                    return true;
+                }
+                break;
         }
         return false;
     }
