f664930f3a8a62da1fe1761ed815d0939aaf2ca5
Iwo Banas <banas.iwo@gmail.com>
2015-02-24 22:06:14 +0100

    pinch-zoom overlay window scaling

--- ScreenRecorder/src/main/java/com/iwobanas/screenrecorder/WindowDragListener.java
+++ ScreenRecorder/src/main/java/com/iwobanas/screenrecorder/WindowDragListener.java
@@ -13,7 +13,7 @@ class WindowDragListener implements View.OnTouchListener {
     private int dragStartX;
     private int dragStartY;
     private boolean dragging;
-    private WindowManager.LayoutParams params;
+    protected final WindowManager.LayoutParams params;
 
     private OnWindowDragStartListener startListener;
     private OnWindowDragEndListener endListener;
@@ -29,77 +29,102 @@ class WindowDragListener implements View.OnTouchListener {
      */
 
     @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
+    public boolean onTouch(final View view, MotionEvent motionEvent) {
         switch (motionEvent.getActionMasked()) {
             case MotionEvent.ACTION_DOWN:
-            case MotionEvent.ACTION_MOVE:
                 if (!dragging) {
                     dragStartX = (int) motionEvent.getX();
                     dragStartY = (int) motionEvent.getY();
-                    if (startListener != null) {
-                        startListener.onDragStart();
-                    }
+                    Log.v(TAG, "Start drag " + dragStartX + ":" +dragStartY);
                     dragging = true;
+                    notifyDragStart();
                     return true;
                 }
-
-                Rect frame = new Rect();
-                view.getWindowVisibleDisplayFrame(frame);
-
-                int leftX, topY, centerX, centerY, rightX, bottomY, gravity = 0;
-
-                leftX = (int) motionEvent.getRawX() - dragStartX - frame.left;
-                centerX = (int) motionEvent.getRawX() - dragStartX - (frame.centerX() - view.getWidth() / 2);
-                rightX = frame.right - (int) motionEvent.getRawX() - view.getWidth() + dragStartX;
-
-                topY = (int) motionEvent.getRawY() - dragStartY - frame.top;
-                centerY = (int) motionEvent.getRawY() - dragStartY - (frame.centerY() - view.getHeight() / 2);
-                bottomY = frame.bottom - (int) motionEvent.getRawY() - view.getHeight() + dragStartY;
-
-                if (leftX <= Math.abs(centerX) && leftX <= rightX) {
-                    params.x = Math.max(leftX, 0);
-                    gravity |= Gravity.LEFT;
-                } else if (rightX <= Math.abs(centerX) && rightX <= leftX) {
-                    params.x = Math.max(rightX, 0);
-                    gravity |= Gravity.RIGHT;
-                } else {
-                    params.x = centerX;
-                    gravity |= Gravity.CENTER_HORIZONTAL;
-                }
-
-                if (topY <= Math.abs(centerY) && topY <= bottomY) {
-                    params.y = Math.max(topY, 0);
-                    gravity |= Gravity.TOP;
-                } else if (bottomY <= Math.abs(centerY) && bottomY <= topY) {
-                    params.y = Math.max(bottomY, 0);
-                    gravity |= Gravity.BOTTOM;
-                } else {
-                    params.y = centerY;
-                    gravity |= Gravity.CENTER_VERTICAL;
-                }
-
-                params.gravity = gravity;
-
-                try {
-                    getWindowManager(view.getContext()).updateViewLayout(view, params);
-                } catch (IllegalArgumentException e) {
-                    Log.e(TAG, "Error updating layout", e);
-                    return false;
-                }
-                return true;
+                // fall through
+            case MotionEvent.ACTION_MOVE:
+                float x = motionEvent.getRawX() - dragStartX;
+                float y = motionEvent.getRawY() - dragStartY;
+                return setGravityAndPosition(view, x, y);
 
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_CANCEL:
                 if (dragging) {
                     dragging = false;
-                    if (endListener != null) {
-                        endListener.onDragEnd();
-                    }
+                    notifyDragEnd();
                     return true;
                 }
                 break;
         }
         return false;
+    }
+
+    protected boolean setGravityAndPosition(View view, float x, float y) {
+        Rect frame = new Rect();
+        view.getWindowVisibleDisplayFrame(frame);
+
+        int leftX, topY, centerX, centerY, rightX, bottomY, gravity = 0;
+
+        leftX = (int) (x - frame.left);
+        centerX = (int) (x - (frame.centerX() - view.getWidth() / 2));
+        rightX = (int) (frame.right - x - view.getWidth());
+
+        topY = (int) (y - frame.top);
+        centerY = (int) (y - (frame.centerY() - view.getHeight() / 2));
+        bottomY = (int) (frame.bottom - y - view.getHeight());
+
+        if (leftX <= Math.abs(centerX) && leftX <= rightX) {
+            params.x = Math.max(leftX, 0);
+            gravity |= Gravity.LEFT;
+        } else if (rightX <= Math.abs(centerX) && rightX <= leftX) {
+            params.x = Math.max(rightX, 0);
+            gravity |= Gravity.RIGHT;
+        } else {
+            params.x = centerX;
+            gravity |= Gravity.CENTER_HORIZONTAL;
+        }
+
+        if (topY <= Math.abs(centerY) && topY <= bottomY) {
+            params.y = Math.max(topY, 0);
+            gravity |= Gravity.TOP;
+        } else if (bottomY <= Math.abs(centerY) && bottomY <= topY) {
+            params.y = Math.max(bottomY, 0);
+            gravity |= Gravity.BOTTOM;
+        } else {
+            params.y = centerY;
+            gravity |= Gravity.CENTER_VERTICAL;
+        }
+        params.gravity = gravity;
+
+        return updateViewLayout(view);
+    }
+
+    protected boolean interruptDrag() {
+        if (!dragging)
+            return false;
+        dragging = false;
+        return true;
+    }
+
+    protected boolean updateViewLayout(View view) {
+        try {
+            getWindowManager(view.getContext()).updateViewLayout(view, params);
+        } catch (IllegalArgumentException e) {
+            Log.e(TAG, "Error updating layout", e);
+            return false;
+        }
+        return true;
+    }
+
+    protected void notifyDragStart() {
+        if (startListener != null) {
+            startListener.onDragStart();
+        }
+    }
+
+    protected void notifyDragEnd() {
+        if (endListener != null) {
+            endListener.onDragEnd();
+        }
     }
 
     private WindowManager getWindowManager(Context context) {
@@ -122,3 +147,4 @@ class WindowDragListener implements View.OnTouchListener {
         void onDragEnd();
     }
 }
+
---
+++ ScreenRecorder/src/main/java/com/iwobanas/screenrecorder/WindowPinchListener.java
@@ -0,0 +1,218 @@
+package com.iwobanas.screenrecorder;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+public class WindowPinchListener extends WindowDragListener implements ScaleGestureDetector.OnScaleGestureListener {
+
+    private static final String TAG = "scr_WindowPinchListener";
+
+    private ScaleGestureDetector scaleGestureDetector;
+    private Handler handler;
+
+    private View view;
+    private View contentView;
+    private ViewGroup.MarginLayoutParams contentViewParams;
+    private boolean scaling;
+    private float focusRatioX;
+    private float focusRatioY;
+    private float width;
+    private float height;
+
+
+    WindowPinchListener(Context context, WindowManager.LayoutParams params) {
+        super(params);
+        handler = new Handler();
+        scaleGestureDetector = new ScaleGestureDetector(context, this);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (view == null || view != v) {
+            view = v;
+            contentView = view.findViewById(R.id.content);
+
+            if (contentView == null) {
+                Log.e(TAG, "Missing content view container");
+                return false;
+            }
+            contentViewParams = (ViewGroup.MarginLayoutParams) contentView.getLayoutParams();
+            if (contentViewParams == null) {
+                contentViewParams = new ViewGroup.MarginLayoutParams(params.width, params.height);
+            }
+        }
+        scaleGestureDetector.onTouchEvent(event);
+        if (scaling) {
+            return true;
+        }
+        return super.onTouch(v, event);
+    }
+
+    @Override
+    public boolean onScaleBegin(ScaleGestureDetector detector) {
+        Log.v(TAG, "Starting scaling");
+
+        boolean dragInterrupted = interruptDrag();
+        scaling = true;
+        makeWindowFullScreen();
+        focusRatioX = detector.getFocusX() / view.getWidth();
+        focusRatioY = detector.getFocusY() / view.getHeight();
+        width = params.width;
+        height = params.height;
+
+        if (!dragInterrupted) {
+            notifyDragStart();
+        }
+        return true;
+    }
+
+    /**
+     * Resizing window is extremely slow and triggers some animations.
+     * As a workaround we create use full-screen window during resizing
+     * and modify a content view within that window.
+     */
+    private void makeWindowFullScreen() {
+
+        contentViewParams.leftMargin = getViewX();
+        contentViewParams.topMargin = getViewY();
+        contentViewParams.width = params.width;
+        contentViewParams.height = params.height;
+        contentView.setLayoutParams(contentViewParams);
+        contentView.setVisibility(View.INVISIBLE);
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                contentView.setVisibility(View.VISIBLE);
+                params.x = 0;
+                params.y = 0;
+                params.gravity = Gravity.LEFT | Gravity.TOP;
+                params.width = WindowManager.LayoutParams.MATCH_PARENT;
+                params.height = WindowManager.LayoutParams.MATCH_PARENT;
+                updateViewLayout(view);
+                Log.v(TAG, "Start transition finished");
+            }
+        });
+    }
+
+    private int getViewX() {
+        Rect frame = new Rect();
+        view.getWindowVisibleDisplayFrame(frame);
+        int gravity = params.gravity & Gravity.HORIZONTAL_GRAVITY_MASK;
+        if (gravity == Gravity.CENTER_HORIZONTAL)
+            return frame.centerX() + params.x - view.getWidth() / 2 - frame.left;
+        if (gravity == Gravity.RIGHT)
+            return frame.right - params.x - view.getWidth() - frame.left;
+        return params.x;
+    }
+
+    private int getViewY() {
+        Rect frame = new Rect();
+        view.getWindowVisibleDisplayFrame(frame);
+        int gravity = params.gravity & Gravity.VERTICAL_GRAVITY_MASK;
+        if (gravity == Gravity.CENTER_VERTICAL)
+            return frame.centerY() + params.y - view.getHeight() / 2 - frame.top;
+        if (gravity == Gravity.BOTTOM)
+            return frame.bottom - params.y - view.getHeight() - frame.top;
+        return params.y;
+    }
+
+    @Override
+    public boolean onScale(ScaleGestureDetector detector) {
+        float scale = detector.getScaleFactor();
+
+        if (scale > 1.1f) {
+            Log.v(TAG, "Ignoring excessive scale update: " + detector.getScaleFactor());
+            return true;
+        }
+
+        final Rect frame = new Rect();
+        view.getWindowVisibleDisplayFrame(frame);
+
+        width *= scale;
+        height *= scale;
+
+        int w = (int) width;
+        int h = (int) height;
+        int x = (int) (detector.getFocusX() - width * focusRatioX);
+        int y = (int) (detector.getFocusY() - height * focusRatioY);
+
+        if (w > frame.width()) {
+            w = frame.width();
+            h = (int) (frame.width() * height / width);
+        }
+
+        if (h > frame.height()) {
+            h = frame.height();
+            w = (int) (frame.height() * width / height);
+        }
+
+        if (x < 0) {
+            x = 0;
+        } else if ((x + w) > frame.width()) {
+            x = frame.width() - w;
+        }
+
+        if (y < 0) {
+            y = 0;
+        }
+        if ((y + h) > frame.height()) {
+            y = frame.height() - h;
+        }
+
+        contentViewParams.leftMargin = x;
+        contentViewParams.topMargin = y;
+        contentViewParams.width = w;
+        contentViewParams.height = h;
+        contentView.setLayoutParams(contentViewParams);
+
+        return true;
+    }
+
+    @Override
+    public void onScaleEnd(ScaleGestureDetector detector) {
+        Log.v(TAG, "Scaling finished");
+        shrinkFullScreenWindow();
+        scaling = false;
+        notifyDragEnd();
+    }
+
+    private void shrinkFullScreenWindow() {
+
+        final float originalAlpha = params.alpha;
+
+        params.width = (int) width;
+        params.height = (int) height;
+        params.alpha = 0;
+        updateViewLayout(view);
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Rect frame = new Rect();
+                view.getWindowVisibleDisplayFrame(frame);
+
+                int x = contentViewParams.leftMargin + frame.left;
+                int y = contentViewParams.topMargin + frame.top;
+
+                contentViewParams.leftMargin = 0;
+                contentViewParams.topMargin = 0;
+                contentViewParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                contentViewParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
+                contentView.setLayoutParams(contentViewParams);
+
+                params.alpha = originalAlpha;
+                setGravityAndPosition(view, x, y);
+                Log.v(TAG, "End transition finished");
+            }
+        });
+    }
+}
